91f5befa4af05563f61a1509babae79e0b39ac64
hampelm <hampelm>
2012-10-03 20:44:07 -0700

    First commit

---
+++ server.py
@@ -0,0 +1,15 @@
+# On request
+	# Get list of facts
+	# If current fact exists
+	# Get list of numbers
+	# For each numbers
+		# Send the facts
+
+
+# On message in 
+	# If message from Matt
+		# Broadcast message
+	# Else
+		# Send Matt the message
+		
+
