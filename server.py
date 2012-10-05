12d24bca9a2678f4c3bfa54f7e435039324af31a
hampelm <hampelm>
2012-10-05 12:33:42 -0700

    Add requirements, server, readme

--- server.py
+++ server.py
@@ -1,3 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
+
 # On request
 	# Get list of facts
 	# If current fact exists
