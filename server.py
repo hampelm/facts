44181099b45248e4d8c77fdb3713b93647e84bfb
hampelm <hampelm>
2012-10-07 13:16:53 -0700

    Nearly there...

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 # Capybara Facts!
 # To load environment variables:
-# os.environ['twilio_account'], os.environ['twilio_token']
+# os.environ['TWILIO_ACCOUNT'], os.environ['TWILIO_TOKEN']
 
+import datetime
 import os
 
 from flask import Flask
+from flask import request
 from flask.ext.sqlalchemy import SQLAlchemy
 from twilio.rest import TwilioRestClient
 
@@ -12,13 +14,16 @@ from twilio.rest import TwilioRestClient
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['POSTGRES_URL']
 db = SQLAlchemy(app)
+client = TwilioRestClient(os.environ['TWILIO_ACCOUNT'], 
+    os.environ['TWILIO_TOKEN'])
 
 # Set up the simple state tracking we need: people and facts.
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     phone = db.Column(db.String(15))
-    last_fact = db.Column(db.Integer)
+    next_fact = db.Column(db.Integer)
+    last_message_datetime = db.Column(db.DateTime(timezone=False))
 
     def __init__(self, name, phone):
         self.name = name
@@ -31,7 +36,7 @@ class Person(db.Model):
 
 class Fact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fact = db.Column(db.String(256))
+    fact = db.Column(db.String(160))
     fact_number = db.Column(db.Integer)
 
     def __init__(self, fact, fact_number):
@@ -42,47 +47,76 @@ class Fact(db.Model):
         return '<Fact %r>' % self.fact
 
 
+# Create the tables if needed 
+db.create_all()
+
+
+# Send out some facts!!
 @app.route("/")
 def hello():
-    facts = Fact.query.all()
-
-    return "Hello World!"
-
-    facts = Fact.query.all()
-    people = User.query.all()
+    facts = Fact.query.order_by(Fact.fact_number).all()
+    number_of_facts = len(facts)
+    people = Person.query.all()
 
     for person in people:
-        next_fact = person.last_fact + 1
+        print person.next_fact
+        print number_of_facts
 
-        fact_to_send = facts[next_fact]
+        # If there is another fact for this person, send it to them
+        # Otherwise, dont do anything.
+        if person.next_fact < number_of_facts:          
+            fact_to_send = facts[person.next_fact].fact
 
-        person.last_fact = next_fact
-        person.save()
+            # Send the fact
+            message = client.sms.messages.create(to=person.phone, from_=os.environ['TWILIO_NUMBER'],
+                                                    body=fact_to_send)
+
+            # Update the database
+            person.next_fact = person.next_fact + 1
+            db.session.add(person)
+            db.session.commit()
+
+
+    return "Capybaras for life!"
+
+@app.route('/twilio', methods=['POST'])
+def incoming():
+    if request.method == 'POST':
+        from_phone = request.form['From']
+        body = request.form['Body']
+
+        if from_phone == os.environ['MY_NUMBER']:
+            # If the message is from the admin, we need to find out 
+            # who to send it on to.
+
+            target = Person.query.filter(Person.last_message_datetime != None).order_by(Person.last_message_datetime.desc()).first()
+            print target.phone
+            print target.name
+
+            if target is not None:
+                print "Forwarding message FROM the admin"
+                message = client.sms.messages.create(to=target.phone, 
+                    from_=os.environ['TWILIO_NUMBER'], body = body)
+
+        else:
+            # Set this person as the origin of the most recent message
+            from_person = Person.query.filter(Person.phone == from_phone).first()
+            print from_person.name 
+            from_person.last_message_datetime = datetime.datetime.now()
+            db.session.add(from_person)
+            db.session.commit()
+
+            print from_person.last_message_datetime
+            print from_person.name
+
+            # Forward the message to the admin
+            print "Forwarding message TO the admin"
+            message = client.sms.messages.create(to=os.environ['MY_NUMBER'], 
+                from_=os.environ['TWILIO_NUMBER'], body=body)
+
+
+    return ""
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# Database structure
-	# Facts
-		# Fact number
-		# Fact
-	# People
-		# Phone number
-		# Last fact number
-
-# On request
-	# Get list of facts
-	# If current fact exists
-	# Get list of numbers
-	# For each numbers
-		# Send the facts
-
-
-# On message in 
-	# If message from Matt
-		# Broadcast message
-	# Else
-		# Send Matt the message
-		
-
--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,5 @@
 export TWILIO_ACCOUNT=""
 export TWILIO_TOKEN=""
+export TWILIO_NUMBER="" # Must include the +
 
 export POSTGRES_URL="postgresql://user:pass@localhost/mydatabase"
